9ebf34736cf6b038a93a31aec47ef20db19b22c4
zooq <michal.zuczkiewicz@profil-software.com>
2019-10-24 09:14:46 +0200

    Initial Commit

---
+++ cardsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cardsApp
+//
+//  Created by hacku on 24/10/2019.
+//  Copyright © 2019 Michał Żuczkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
